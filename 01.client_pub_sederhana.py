0e9fb97fee6aae2adc6c3442d756ea426c667b77
Rizky Ilhamsyah <54507408+superizky27@users.noreply.github.com>
2020-02-25 11:05:50 +0700

    2nd, commiting for no 1
    Belum selesai

--- 01.client_pub_sederhana.py
+++ 01.client_pub_sederhana.py
@@ -1,34 +1,44 @@
 # import paho mqtt
+import paho.mqtt.client as mqtt
 
 
 # import time untuk sleep()
-
+import time
 
 # import datetime untuk mendapatkan waktu dan tanggal
-
-
+import datetime
+#now = datetime.datetime.now()
+#def on_publish(client,userdata,result):             #create function for callback
+#    print(str(now))
+#    pass
 
 # definisikan nama broker yang akan digunakan
-
+broker = "127.0.0.1"
 
 # buat client baru bernama P2
 print("creating new instance")
+client = mqtt.Client("P1")
 
 #client.on_publish=on_publish
 
 # koneksi ke broker
 print("connecting to broker")
+client.connect(broker, port=1883)
 
 
 # mulai loop client
-
-
+client.loop_start()
+print("Subscribing to topic","house/bulbs/bulb1")
 # lakukan 20x publish waktu dengan topik 1
 print("publish something")
 for i in range (20):
     # sleep 1 detik
-    
-    # publish waktu sekarang
-    
+    time.sleep(1)
 
+    # publish waktu sekarang
+    waktu = datetime.datetime.now()
+    print("Publishing message to topic","waktu")
+    client.publish("waktu","OFF")
+	
 #stop loop
+client.loop_stop()
--- 01.client_sub_sederhana.py
+++ 01.client_sub_sederhana.py
@@ -1,7 +1,9 @@
 # import paho mqtt
+import paho.mqtt.client as mqtt
 
 
 # import time for sleep()
+import time
 
 
 # buat callback on_message; jika ada pesan
@@ -9,32 +11,41 @@
 ########################################
 def on_message(client, userdata, message):
     # print pesan
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
     
 ########################################
     
 # buat definisi nama broker yang akan digunakan
-
+broker = "127.0.0.1"
 
 # buat client baru bernama P1
 print("creating new instance")
-
+client = mqtt.Client("P1")
 
 # kaitkan callback on_message ke client
-
+client.on_message = on_message
 
 # buat koneksi ke broker
 print("connecting to broker")
-
+client.connect(broker, port=1883)
+print("connected!")
 
 # jalankan loop client
+client.loop_start()
 
 
 # client melakukan subsribe ke topik 1
-
+print("Subscribing to topic","waktu")
+client.subscribe("waktu")
 
 # loop forever
 while True:
     # berikan waktu tunggu 1 detik 
-    
+    time.sleep(1)
+
 
 #stop loop
+client.loop_stop()
